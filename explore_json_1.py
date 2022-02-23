6b2c64f2087c739a809c58c7e5024c3c9b03e691
Lyndsey <lyndsey_davis2@baylor.edu>
2022-02-23 14:05:12 -0600

    class 2/23

---
+++ explore_json_1.py
@@ -0,0 +1,38 @@
+import json
+
+infile = open("eq_data_1_day_m1.json", "r")
+outfile = open("readable_eq_data.json", "w")
+
+# converts json to python obj
+eq_data = json.load(infile)
+
+json.dump(eq_data, outfile, indent=4)
+
+# create list of earthquakes
+list_of_eqs = eq_data["features"]
+print(len(list_of_eqs))
+
+# create list of magnitudes, longitudes, latitudes
+mags, lons, lats = [], [], []
+for eq in list_of_eqs:
+    mag = eq["properties"]["mag"]
+    mags.append(mag)
+    lon = eq["geometry"]["coordinates"][0]
+    lons.append(lon)
+    lat = eq["geometry"]["coordinates"][1]
+    lats.append(lat)
+print(mags[:10])
+print(lons[:10])
+print(lats[:10])
+
+# create graph
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+
+data = [Scattergeo(lon=lons, lat=lats)]
+
+my_layout = Layout(title="Global Earthquakes")
+
+fig = {"data": data, "layout": my_layout}
+
+offline.plot(fig, filename="global_earthquakes.html")
